b6bb1469cac9d5df5bf4f61b55f7261accd64163
Christine Matthews <candreola@gmail.com>
2024-07-07 16:38:13 -0500

    convert city to number and console data for that city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,41 @@
 const searchBtn = document.getElementById('search-btn');
-const city = document.getElementById('city')
-const state = document.getElementById('state')
-const cityEx = 'London';
+const city = document.getElementById('city');
+const state = document.getElementById('state');
 const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
-const lat = '37.7749'
-const lon = '-122.4194'
-const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
-const cityUrl = `https://api.api-ninjas.com/v1/geocoding?city=${cityEx}&X-Api-Key=VgssbW4kdRpWeJjECntvng==nBgdtI7KDrVd5JZc`
+let lat = '37.7749'
+let lon = '-122.4194'
 
 
 
 
 searchBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    // console.log("clicked")
-
-//    console.log(city.value)
-getCityWeather();
- 
-   
+    convertCityToNum()
+    getCityWeather();
+    
+    
+    
 })
 
+function convertCityToNum () {
+    const cityUrl = `https://api.api-ninjas.com/v1/geocoding?city=${city.value}&state=${state.value}&country=${country.value}&X-Api-Key=VgssbW4kdRpWeJjECntvng==nBgdtI7KDrVd5JZc`
+    fetch (cityUrl)
+    .then(response => response.json())
+    .then(data => {
+        lat = data[0].latitude
+        lon = data[0].longitude
+        // console.log(lat, lon)
+    })
+    .catch(error => {
+        console.error('error fetching data:', error)
+    })
+    
+}
 
 
 function getCityWeather () {
-    fetch(cityUrl) 
+    let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    fetch(url) 
     .then(response => response.json())
     .then(data => {
         console.log(data);

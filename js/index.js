306bbedd73c9f9ff225ff03698624e32bbe75404
Christine Matthews <candreola@gmail.com>
2024-07-20 15:36:46 -0500

    Pull weather data and display on page

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ function convertZip () {
                 // console.log(city.city)
                 setLocalStorage(city)
                 convertToNum(city)
-                createCurrentWeather(city)
+                createCurrentWeatherName(city)
             }
         })
         .catch(error => {
@@ -45,7 +45,7 @@ function setLocalStorage (cityName) {
     readStorage()
     
     
-    const cityObj = {city: cityName.city}
+    const cityObj = {city: cityInput.value}
     
     
     // console.log(cityObj)
@@ -69,7 +69,12 @@ function convertToNum (cityName) {
             lon = data[0].longitude
             // console.log(lat, lon)
             getCurrentWeather(lat, lon)
-            // getDailyWeather(lat, lon)
+            getTomorrowWeather(lat, lon)
+            getTwoWeather(lat, lon)
+            getThreeWeather(lat, lon)
+            getFourWeather(lat, lon)
+            getFiveWeather(lat, lon)
+
 
          
     
@@ -84,6 +89,8 @@ function convertToNum (cityName) {
         
 }
 
+
+
 function getCurrentWeather() {
     let currentDate = dayjs().format('YYYY-MM-DD')
     const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
@@ -92,7 +99,12 @@ function getCurrentWeather() {
     .then(response => response.json())
     .then(data => {
         console.log(data)
+        sun = data.cloud_cover.afternoon
+        temp = data.temperature.max
+        wind = data.wind.max.speed
+        humid = data.humidity.afternoon
         
+        createCurrentWeatherData(sun, temp, wind, humid)
 
 
     })
@@ -102,11 +114,338 @@ function getCurrentWeather() {
 
 }
 
-function createCurrentWeather (cityName) {
+
+
+function createCurrentWeatherName (cityName) {
+    const weather = document.getElementById('weather')
+    weather.classList.remove('d-none')
+    // city name
     const cityToday = cityName.city
     const h2 = document.getElementById('city-today')
     h2.textContent = `${cityToday} Today`
-
     
+
 }
+
+function createCurrentWeatherData () {
+
+       // icon and sun info
+       const icon = document.getElementById('icon')
+       const sunInfo = document.getElementById('sun-info')
+       if (sun > 90) {
+           icon.setAttribute('src', './icons/brightness-high.svg')
+           sunInfo.textContent = 'The sun is out today!'
+       } else if (sun <= 90 && sun > 10) {
+           icon.setAttribute('src', './icons/cloud-sun.svg')
+           sunInfo.textContent = 'The sun is partially out today!'
+       } else if (sun <= 10) {
+           icon.setAttribute('src', './icons/cloud.svg')
+           sunInfo.textContent = 'The sun is behind the clouds today!'
+       }
+
+
+    //    Ul info
+
+    const tempInfo = document.getElementById('temp')
+    const windInfo = document.getElementById('wind')
+    const humidInfo = document.getElementById('humid');
+
+    tempInfo.textContent = `Temp: ${temp} °F`
+    windInfo.textContent = `Wind: ${wind}mph`
+    humidInfo.textContent = `Humidity: ${humid}%`
+
+
+}
+
+
+function getTomorrowWeather () {
+    let tomorrow = dayjs().add(1, 'day').format('YYYY-MM-DD') 
+    const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
+    let url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${tomorrow}&units=imperial&appid=${apiKey}`
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        sun = data.cloud_cover.afternoon
+        temp = data.temperature.max
+        wind = data.wind.max.speed
+        humid = data.humidity.afternoon
+        
+        createTomorrowWeatherData(sun, temp, wind, humid)
+
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    })
+}
+
+
+
+function createTomorrowWeatherData () {
+    let tomorrowFormat = dayjs().add(1, 'day').format('dddd') 
+
+    const tomorrowName = document.getElementById('tomorrow-name')
+    
+    tomorrowName.textContent = tomorrowFormat
+    
+    // icon and sun info
+    const tomorrowIcon = document.getElementById('tomorrow-icon')
+
+       if (sun > 90) {
+           tomorrowIcon.setAttribute('src', './icons/brightness-high.svg')
+       } else if (sun <= 90 && sun > 10) {
+           tomorrowIcon.setAttribute('src', './icons/cloud-sun.svg')
+       } else if (sun <= 10) {
+           tomorrowIcon.setAttribute('src', './icons/cloud.svg')
+       }
+    
+
+
+    //    Ul info
+
+    const tempInfo = document.getElementById('tomorrow-temp')
+    const windInfo = document.getElementById('tomorrow-wind')
+    const humidInfo = document.getElementById('tomorrow-humid');
+
+    tempInfo.textContent = `Temp: ${temp} °F`
+    windInfo.textContent = `Wind: ${wind}mph`
+    humidInfo.textContent = `Humidity: ${humid}%`
+
+
+}
+
+function getTwoWeather () {
+    let twoDays = dayjs().add(2, 'day').format('YYYY-MM-DD')
+    const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
+    let url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${twoDays}&units=imperial&appid=${apiKey}`
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        sun = data.cloud_cover.afternoon
+        temp = data.temperature.max
+        wind = data.wind.max.speed
+        humid = data.humidity.afternoon
+        
+        createTwoWeatherData(sun, temp, wind, humid)
+
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    })
+}
+
+
+
+function createTwoWeatherData () {
+    let twoDays = dayjs().add(2, 'day').format('dddd')
+
+    const twoName = document.getElementById('two-name')
+    
+    twoName.textContent = twoDays
+    
+    // icon and sun info
+    const twoIcon = document.getElementById('two-icon')
+
+       if (sun > 90) {
+           twoIcon.setAttribute('src', './icons/brightness-high.svg')
+       } else if (sun <= 90 && sun > 10) {
+           twoIcon.setAttribute('src', './icons/cloud-sun.svg')
+       } else if (sun <= 10) {
+           twoIcon.setAttribute('src', './icons/cloud.svg')
+       }
+    
+
+
+    //    Ul info
+
+    const tempInfo = document.getElementById('two-temp')
+    const windInfo = document.getElementById('two-wind')
+    const humidInfo = document.getElementById('two-humid');
+
+    tempInfo.textContent = `Temp: ${temp} °F`
+    windInfo.textContent = `Wind: ${wind}mph`
+    humidInfo.textContent = `Humidity: ${humid}%`
+
+
+}
+
+function getThreeWeather () {
+    let threeDays = dayjs().add(3, 'day').format('YYYY-MM-DD')
+    const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
+    let url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${threeDays}&units=imperial&appid=${apiKey}`
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        sun = data.cloud_cover.afternoon
+        temp = data.temperature.max
+        wind = data.wind.max.speed
+        humid = data.humidity.afternoon
+        
+        createThreeWeatherData(sun, temp, wind, humid)
+
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    })
+}
+
+
+
+function createThreeWeatherData () {
+    let threeDays = dayjs().add(3, 'day').format('dddd')
+
+    const threeName = document.getElementById('three-name')
+    
+    threeName.textContent = threeDays
+    
+    // icon and sun info
+    const threeIcon = document.getElementById('three-icon')
+
+       if (sun > 90) {
+           threeIcon.setAttribute('src', './icons/brightness-high.svg')
+       } else if (sun <= 90 && sun > 10) {
+           threeIcon.setAttribute('src', './icons/cloud-sun.svg')
+       } else if (sun <= 10) {
+           threeIcon.setAttribute('src', './icons/cloud.svg')
+       }
+    
+
+
+    //    Ul info
+
+    const tempInfo = document.getElementById('three-temp')
+    const windInfo = document.getElementById('three-wind')
+    const humidInfo = document.getElementById('three-humid');
+
+    tempInfo.textContent = `Temp: ${temp} °F`
+    windInfo.textContent = `Wind: ${wind}mph`
+    humidInfo.textContent = `Humidity: ${humid}%`
+
+
+}
+
+function getFourWeather () {
+    let fourDays = dayjs().add(4, 'day').format('YYYY-MM-DD')
+    const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
+    let url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${fourDays}&units=imperial&appid=${apiKey}`
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        sun = data.cloud_cover.afternoon
+        temp = data.temperature.max
+        wind = data.wind.max.speed
+        humid = data.humidity.afternoon
+        
+        createFourWeatherData(sun, temp, wind, humid)
+
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    })
+}
+
+
+
+function createFourWeatherData () {
+    let fourDays = dayjs().add(4, 'day').format('dddd')
+
+    const fourName = document.getElementById('four-name')
+    
+    fourName.textContent = fourDays
+    
+    // icon and sun info
+    const fourIcon = document.getElementById('four-icon')
+
+       if (sun > 90) {
+           fourIcon.setAttribute('src', './icons/brightness-high.svg')
+       } else if (sun <= 90 && sun > 10) {
+           fourIcon.setAttribute('src', './icons/cloud-sun.svg')
+       } else if (sun <= 10) {
+           fourIcon.setAttribute('src', './icons/cloud.svg')
+       }
+    
+
+
+    //    Ul info
+
+    const tempInfo = document.getElementById('four-temp')
+    const windInfo = document.getElementById('four-wind')
+    const humidInfo = document.getElementById('four-humid');
+
+    tempInfo.textContent = `Temp: ${temp} °F`
+    windInfo.textContent = `Wind: ${wind}mph`
+    humidInfo.textContent = `Humidity: ${humid}%`
+
+
+}
+
+function getFiveWeather () {
+    let fiveDays = dayjs().add(5, 'day').format('YYYY-MM-DD')
+    const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
+    let url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${fiveDays}&units=imperial&appid=${apiKey}`
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        sun = data.cloud_cover.afternoon
+        temp = data.temperature.max
+        wind = data.wind.max.speed
+        humid = data.humidity.afternoon
+        
+        createFiveWeatherData(sun, temp, wind, humid)
+
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    })
+}
+
+
+
+function createFiveWeatherData () {
+    let fiveDays = dayjs().add(5, 'day').format('dddd')
+
+    const fiveName = document.getElementById('five-name')
+    
+    fiveName.textContent = fiveDays
+    
+    // icon and sun info
+    const fiveIcon = document.getElementById('five-icon')
+
+       if (sun > 90) {
+           fiveIcon.setAttribute('src', './icons/brightness-high.svg')
+       } else if (sun <= 90 && sun > 10) {
+           fiveIcon.setAttribute('src', './icons/cloud-sun.svg')
+       } else if (sun <= 10) {
+           fiveIcon.setAttribute('src', './icons/cloud.svg')
+       }
+    
+
+
+    //    Ul info
+
+    const tempInfo = document.getElementById('five-temp')
+    const windInfo = document.getElementById('five-wind')
+    const humidInfo = document.getElementById('five-humid');
+
+    tempInfo.textContent = `Temp: ${temp} °F`
+    windInfo.textContent = `Wind: ${wind}mph`
+    humidInfo.textContent = `Humidity: ${humid}%`
+
+
+}
+
+
+
+
+
+
 

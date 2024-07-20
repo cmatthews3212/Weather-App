f9460aee1aaf4e052e055c8ce04bddd397d70547
Christine Matthews <candreola@gmail.com>
2024-07-20 14:17:04 -0500

    Pull from weather data

--- js/index.js
+++ js/index.js
@@ -19,6 +19,8 @@ function convertZip () {
             for(const city of data) {
                 // console.log(city.city)
                 setLocalStorage(city)
+                convertToNum(city)
+                createCurrentWeather(city)
             }
         })
         .catch(error => {
@@ -54,3 +56,57 @@ function setLocalStorage (cityName) {
 
 
 }
+
+function convertToNum (cityName) {
+        const city = cityName.city
+        
+        const cityUrl = `https://api.api-ninjas.com/v1/geocoding?city=${city}&X-Api-Key=VgssbW4kdRpWeJjECntvng==nBgdtI7KDrVd5JZc`
+        fetch (cityUrl)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            lat = data[0].latitude
+            lon = data[0].longitude
+            // console.log(lat, lon)
+            getCurrentWeather(lat, lon)
+            // getDailyWeather(lat, lon)
+
+         
+    
+       
+    
+            
+        })
+        .catch(error => {
+            console.error('error fetching data:', error)
+        })
+
+        
+}
+
+function getCurrentWeather() {
+    let currentDate = dayjs().format('YYYY-MM-DD')
+    const apiKey = 'ccf6ee45f4426f5df18513805a5e47df';
+    let url = `https://api.openweathermap.org/data/3.0/onecall/day_summary?lat=${lat}&lon=${lon}&date=${currentDate}&units=imperial&appid=${apiKey}`
+    fetch(url) 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        
+
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    })
+
+}
+
+function createCurrentWeather (cityName) {
+    const cityToday = cityName.city
+    const h2 = document.getElementById('city-today')
+    h2.textContent = `${cityToday} Today`
+
+    
+}
+
